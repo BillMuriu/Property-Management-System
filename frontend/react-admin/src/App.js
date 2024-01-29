d78a1f129f36e6fef78c44b8cf8eab153a7b7481
unknown <njorogekamau63@gmail.com>
2024-01-29 10:58:55 +0300

    React pro sidebar

--- frontend/react-admin/src/App.js
+++ frontend/react-admin/src/App.js
@@ -1,22 +1,29 @@
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import { Routes, Route } from "react-router-dom";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import Topbar from "./scenes/global/Topbar"
+
 
 function App() {
-  const [theme, colorMode] = useMode('');
-
-
+  const [theme, colorMode] = useMode();
   return (
-  <ColorModeContext.Provider value={colorMode}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <div className="app">
-        <main className="content">
-          <Topbar />
-        </main>
-      </div>
-    </ThemeProvider>
-  </ColorModeContext.Provider>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            {/* <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes> */}
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+
   );
 }
 
