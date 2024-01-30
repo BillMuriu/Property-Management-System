fee746a517f95af6ec6fa4aed0011c38d78faaf4
Bill <94866000+BillMuriu@users.noreply.github.com>
2024-01-31 01:44:55 +0300

    Update models.py
    link utilities to invoices

--- backend/backend/property/models.py
+++ backend/backend/property/models.py
@@ -151,6 +151,7 @@ class Utilities(models.Model):
         max_length=100, choices=UTILITY_ITEM_CHOICES)
     current_reading = models.DecimalField(max_digits=10, decimal_places=2)
     month = models.CharField(max_length=20, choices=MONTH_CHOICES)
+    #add invoice number.
     previous_reading = models.DecimalField(
         max_digits=10, decimal_places=2, null=True, blank=True)
 
